dcbca3ee3495836e85e76d80a2ee90bc10d46cc7
Kristof <kristof_heulsen@outlook.com>
2019-12-03 08:31:25 +0100

    random

--- ip_repo/NeonIp_1.0/drivers/NeonIp_v1_0/data/NeonIp.tcl
+++ ip_repo/NeonIp_1.0/drivers/NeonIp_v1_0/data/NeonIp.tcl
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 
 
 proc generate {drv_handle} {
 	xdefine_include_file $drv_handle "xparameters.h" "NeonIp" "NUM_INSTANCES" "DEVICE_ID"  "C_S00_AXI_BASEADDR" "C_S00_AXI_HIGHADDR"
 }
+=======
+
+
+proc generate {drv_handle} {
+	xdefine_include_file $drv_handle "xparameters.h" "NeonIp" "NUM_INSTANCES" "DEVICE_ID"  "C_S00_AXI_BASEADDR" "C_S00_AXI_HIGHADDR"
+}
+>>>>>>> Stashed changes
