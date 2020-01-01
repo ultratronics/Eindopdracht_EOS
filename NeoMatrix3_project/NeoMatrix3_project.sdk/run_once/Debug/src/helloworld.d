4543eb0651ac11566c32bc7c78469a9e1e6d5935
Dennis <11702924@student.pxl.be>
2020-01-01 22:10:50 +0100

    probeersel NeoMatrix
    andere manier, waatbij we de lijn laag te houden na het sturen van de data in state 6

---
+++ NeoMatrix3_project/NeoMatrix3_project.sdk/run_once/Debug/src/helloworld.d
@@ -0,0 +1,45 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/NeoMatix64.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/NeoMatix64.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ NeoMatrix3_project/NeoMatrix3_project.sdk/run_once/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../run_once_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../run_once_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../run_once_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
